80411debe1de9ad5ee616187b53f90d2d66f1df5
Jerry Yan <jerry0317@users.noreply.github.com>
2020-07-04 16:04:40 -0700

    Add command line options to change the rcs filters used

--- Sources/JYMT-ML-StructureFinder/main.swift
+++ Sources/JYMT-ML-StructureFinder/main.swift
@@ -19,6 +19,18 @@ printWelcomeBanner("Structure Finder w/ ML")
 
 internal let xyz2mol = importXyz2mol()
 
+var rcsFilterUsed: Set<StrcFilter> = [.minimumBondLength, .bondTypeLength, .valence]
+
+if CommandLine.arguments.count >= 2 {
+    if CommandLine.arguments.contains("-3ppf") {
+        rcsFilterUsed = [.minimumBondLength, .bondTypeLength, .valence]
+        print("[Running with 3 pre-processed filters]\n")
+    } else if CommandLine.arguments.contains("-5ppf") {
+        rcsFilterUsed = [.minimumBondLength, .bondTypeLength, .valence, .bondAngle, .coplanarity]
+        print("[Running with 5 pre-processed filters]\n")
+    }
+}
+
 let numOfThreads = sysconf(CInt(_SC_NPROCESSORS_ONLN)) * 2 - 1
 print("Note: \(numOfThreads) threads will be used for calculation.")
 print()
@@ -32,7 +44,7 @@ print()
 
 let tInitial = Date()
 
-let snt = sntAction(from: xyzFiles, xyz2mol: xyz2mol, chunkSize: numOfThreads, rcsFilters: [.minimumBondLength, .bondTypeLength, .valence])
+let snt = sntAction(from: xyzFiles, xyz2mol: xyz2mol, chunkSize: numOfThreads, rcsFilters: rcsFilterUsed)
 let csvUrl = writePath.appendingPathComponent("result_" + String(Int(tInitial.timeIntervalSince1970)) + ".csv")
 
 exportCsvFile(from: snt, to: csvUrl)
